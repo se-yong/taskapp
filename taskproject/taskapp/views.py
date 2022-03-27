eeb25f8c9c9446a7f371203bce2452a314efe647
se-yong <parksae0428@gmail.com>
2022-03-28 00:15:14 +0900

    페이지네이션 기능 추가 및 템플릿 필터 구현

--- taskproject/taskapp/views.py
+++ taskproject/taskapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, ListView
 from .models import Task, ChecklistItem
 from django.utils import timezone
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -14,8 +15,11 @@ class TaskListView(TemplateView):
 
     def get_context_data(self, **kwargs):
         tasks = Task.objects.filter(due__gte=timezone.now()).order_by('-due').all()
+        paginator = Paginator(tasks, 1)
+        page_number = self.request.GET.get('page', '1')
+        paging = paginator.get_page(page_number)
         return{
-            'tasks': tasks
+            'paging': paging
         }
 
 
@@ -25,8 +29,8 @@ class TaskCreateView(CreateView):
     template_name = 'pages/task_create.html'
     success_url = '/'
 
-
 class TaskPreviousListView(ListView):
     model = Task
     template_name = 'pages/task_previous_list.html'
     queryset = Task.objects.filter(due__lt=timezone.now()).order_by('-due')
+    paginate_by = 4
